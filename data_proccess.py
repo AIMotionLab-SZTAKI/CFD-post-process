2d9ebc03095c939961039984209ebc3e548547f9
Weyny <wkadam9@gmail.com>
2023-05-03 17:13:46 +0200

    CFD dynamic pressure process

--- data_proccess.py
+++ data_proccess.py
@@ -14,11 +14,8 @@ with open(input, 'r') as input_file, open(output, 'w', newline='') as output_fil
     # Write header line to output CSV file
     writer.writerow(next(reader))
 
-    nodes = []
-    x_vals = []
-    y_vals = []
-    z_vals = []
-    pressure_vals = []
+    data=[]
+    sorted_data=[]
 
 
     # Iterate through each row in the input CSV file
@@ -32,12 +29,20 @@ with open(input, 'r') as input_file, open(output, 'w', newline='') as output_fil
         x = x + 0.25
         y = y + 0.25
 
-        x = np.format_float_positional(x,precision=2)
-        y = np.format_float_positional(y,precision=2)
-        z = np.format_float_positional(z,precision=2)
+        x = np.format_float_positional(x,precision=3)
+        y = np.format_float_positional(y,precision=3)
+        z = np.format_float_positional(z,precision=3)
+
+        row_data=[node,x,y,z,pressure]
+        data.append(row_data)
+        #writer.writerow([node,x,y,z,pressure])
+
+    data=np.array(data)
+    sorted_data = data[data[:, 2].argsort(kind='mergesort')]
+    sorted_data = sorted_data[sorted_data[:, 1].argsort(kind='mergesort')]
+    sorted_data = sorted_data[sorted_data[:, 0].argsort(kind='mergesort')]
 
 
-
-        writer.writerow([node, x, y, z, pressure])
+    writer.writerows(sorted_data)
 
 
